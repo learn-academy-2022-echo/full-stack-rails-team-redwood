8daa853f87ea74c97feb0b7cb667ced5b0669054
HeavyArms0511 <learnacademy@LEARNs-MacBook-Air.local>
2022-09-08 10:57:39 -0700

    Update and delete method working, edit adds it to the bottom of list still working on it

--- app/controllers/blog_post_controller.rb
+++ app/controllers/blog_post_controller.rb
@@ -11,7 +11,38 @@ class BlogPostController < ApplicationController
         @post = BlogPost.new
     end    
     def create
-        @post = BlogPost.create name:params[:name], text:params[:text]
+        @post = BlogPost.create(blog_params)
+        if @post.valid?
+            redirect_to blog_posts_path
+        else
+            redirect_to new_blog_post
+        end
+    end
+
+    def edit
+        @post = BlogPost.find(params[:id])
+    end
+
+    def update
+        @post = BlogPost.find(params[:id])
+         @post.update(blog_params)
+        if @post.valid?
+            redirect_to blog_post_path
+        else
+            redirect_to edit_blog_post_path
+        end
+    end
+
+    def destroy
+        @post = BlogPost.find(params[:id])
+        if @post.destroy
+            redirect_to blog_posts_path
+        end
+    end
+
+    private
+    def blog_params
+        params.require(:blog_post).permit(:name, :text)
     end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'blog_post#index'
-  get 'posts' => 'blog_post#index', as: 'posts'
-  get 'posts/new' => 'blog_post#new', as: 'new_post'
-  get 'post/:id' => 'blog_post#show', as: 'post'
+  get 'posts' => 'blog_post#index', as: 'blog_posts'
+  get 'posts/new' => 'blog_post#new', as: 'new_blog_post'
+  get 'posts/:id' => 'blog_post#show', as: 'blog_post'
   post 'posts' => 'blog_post#create'
+  get 'posts/edit/:id' => 'blog_post#edit', as: 'edit_blog_post'
+  patch 'posts/:id' => 'blog_post#update'
+  delete 'posts/:id' => 'blog_post#destroy', as: 'delete_blog_post'
 end
